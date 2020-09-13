1c9d270d6128b05b429d790ef493096ad0a586ff
cjwcommuny <cjwcommuny@outlook.com>
2020-09-13 09:26:34 +0800

    feat: add list_slice of List[Tuple]

--- pybetter/list.py
+++ pybetter/list.py
@@ -1,5 +1,14 @@
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, List, Union
 
 
 def unpack_iterable_of_tuple(iter: Iterable[tuple]) -> Tuple[list, ...]:
     return tuple(list(x) for x in zip(*iter))
+
+
+def list_slice(lst: List[Tuple], key: Union[int, str]) -> list:
+    """
+    e.g.
+    >>> list_slice([(0,1),(1,2),(2,3)], key=0)
+    [0,1,2]
+    """
+    return [x[key] for x in lst]
